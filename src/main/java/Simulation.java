d4a4068798db6900d85658f53bea2a977cf157ca
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2026-01-04 22:30:06 +0100

    feat: Implement initial simulation loop and animal placement

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,2 +1,37 @@
-public class Simulation {
+import model.elements.Herbivore;
+import model.map.RandomAnimalGenerator;
+import model.map.RandomPositionGenerator;
+import model.map.WorldMap;
+
+public class Simulation implements Runnable{
+
+    private WorldMap worldMap;
+    private RandomPositionGenerator randomPositionGenerator;
+    private RandomAnimalGenerator randomAnimalGenerator = new RandomAnimalGenerator();
+    private int startAnimals;
+    private int startGeneLengthAnimals;
+    private int startEnergyAnimal;
+
+
+    public Simulation(WorldMap worldMap, int startAnimals, int startEnergyAnimal, int startGeneLengthAnimals){
+        this.worldMap = worldMap;
+        this.startAnimals = startAnimals;
+        this.startEnergyAnimal = startEnergyAnimal;
+        this.startGeneLengthAnimals = startGeneLengthAnimals;
+        this.randomPositionGenerator =
+                new RandomPositionGenerator(worldMap.getMapUpCorner().getX(),
+                worldMap.getMapUpCorner().getY(), startAnimals);
+    }
+
+    public void run() {
+        randomPositionGenerator.iterator()
+                .forEachRemaining(vector2d
+                        -> worldMap.place(
+                                new Herbivore(0,
+                                        startEnergyAnimal,
+                                        randomAnimalGenerator.getGeneQueue(startGeneLengthAnimals),
+                                        randomAnimalGenerator.getMapDirection(),
+                                        vector2d)));
+        System.out.println(worldMap);
+    }
 }
--- src/main/java/World.java
+++ src/main/java/World.java
@@ -3,7 +3,8 @@ import model.map.WorldMap;
 
 public class World {
     public static void main() {
-        WorldMap worldMap = new WorldMap(new Vector2d(2,2), new Vector2d(4,4),
-                new Vector2d(6,6), 5, 5);
-    }
-}
+        WorldMap worldMap = new WorldMap(new Vector2d(6,6), new Vector2d(2,2),
+                new Vector2d(4,4), 5, 5);
+    Simulation simulation = new Simulation(worldMap, 2, 20, 3);
+    simulation.run();
+}}
