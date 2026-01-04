fe34dd16606297ab376abce926eab280ff015ade
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2026-01-04 22:30:06 +0100

    feat: Add utility methods and string representation to Gene enum

--- src/main/java/model/elements/Gene.java
+++ src/main/java/model/elements/Gene.java
@@ -1,5 +1,7 @@
 package model.elements;
 
+import model.map.MapDirection;
+
 public enum Gene {
     GENE_0,
     GENE_1,
@@ -12,6 +14,35 @@ public enum Gene {
 
     public int getRotation(){
         return ordinal();
+    }
+
+    public Gene next(){
+        int nextOrdinal = (ordinal() + 1) % values().length;
+        return values()[nextOrdinal];
+    }
+
+    public Gene previous(){
+        int nextOrdinal = (ordinal() - 1 +values().length) % values().length;
+        return values()[nextOrdinal];
+    }
+
+    public Gene nextGene(int shift){
+        int nextOrdinal = (ordinal() + shift) % values().length;
+        return values()[nextOrdinal];
+    }
+
+    @Override
+    public String toString() {
+        return switch (this){
+            case GENE_0 -> "Gene_0";
+            case GENE_1 -> "Gene_1";
+            case GENE_2 -> "Gene_2";
+            case GENE_3 -> "Gene_3";
+            case GENE_4 -> "Gene_4";
+            case GENE_5 -> "Gene_5";
+            case GENE_6 -> "Gene_6";
+            case GENE_7 -> "Gene_7";
+        };
     }
 }
 
