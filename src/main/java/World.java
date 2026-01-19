721133d7997731446d2861f9a0c886b113887467
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2026-01-19 16:27:55 +0100

    Config: Update default simulation parameters and add IDE inspection profile

--- src/main/java/World.java
+++ src/main/java/World.java
@@ -5,6 +5,6 @@ public class World {
     public static void main() {
         WorldMap worldMap = new WorldMap(new Vector2d(6,6), new Vector2d(2,2),
                 new Vector2d(4,4), 5, 5);
-    Simulation simulation = new Simulation(worldMap, 2, 20, 3);
+    Simulation simulation = new Simulation(worldMap, 6, 100, 10);
     simulation.run();
 }}
